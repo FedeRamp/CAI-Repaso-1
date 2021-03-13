8b96efa00c612dbc43d55f7aafd4ca9b9898dda5
Federico Rampazzo <federamp91@gmail.com>
2021-03-13 12:58:30 -0300

    Terminado
    Solución terminada, probada.

--- Solucion/Solucion/Program.cs
+++ Solucion/Solucion/Program.cs
@@ -2,11 +2,30 @@
 
 namespace Solucion
 {
+    //Crear loop que vaya de 0 a 227
+    //Ejercicio de Foo - Bar - FooBar
+
     class MainClass
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int fooDiv = 3;
+            int barDiv = 5;
+            int limit = 227;
+
+            for(int i = 0; i < limit; i++)
+            {
+                string resultado = "";
+                if(i % fooDiv == 0)
+                {
+                    resultado += "Foo";
+                }
+                if(i % barDiv == 0)
+                {
+                    resultado += "Bar";
+                }
+                Console.WriteLine(i + "  " + resultado);
+            }
         }
     }
 }
